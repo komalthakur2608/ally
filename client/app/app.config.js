ddb4fdd4a6fb62ab838279c4e103e30eb96e9794
Shubhanshu Aggarwal <sbs.aggarwal1994@live.com>
2016-10-24 11:09:58 +0530

    Configured navbar

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -1,9 +1,13 @@
 'use strict';
 
-export function routeConfig($urlRouterProvider, $locationProvider) {
+export function routeConfig($urlRouterProvider, $locationProvider, $mdThemingProvider) {
   'ngInject';
 
   $urlRouterProvider.otherwise('/');
 
   $locationProvider.html5Mode(true);
+
+  $mdThemingProvider.theme('default')
+    .primaryPalette('cyan')
+    .accentPalette('blue-grey');
 }
